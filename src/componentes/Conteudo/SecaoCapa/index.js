0cdb18e681792e32ed96d452aa475d361b507454
leozizz <leonardocps2015@gmail.com>
2023-02-14 16:23:38 -0300

    Estilização da seção de capa

--- src/componentes/Conteudo/SecaoCapa/index.js
+++ src/componentes/Conteudo/SecaoCapa/index.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function SecaoCapa(){
     return(
         <section className="secao-capa">
-            <div>
-            <p>Preços baixos em</p>
-            <h1>ÓCULOS DE GRAU E DE SOL</h1>
-            <p>Você só encontra aqui</p>
+            <div className="conteudo limitar-secao">
+                <p>Preços baixos em</p>
+                <h1>ÓCULOS DE GRAU E DE SOL</h1>
+                <p>Você só encontra aqui</p>
             </div>
         </section>
     )
