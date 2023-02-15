664cf924f716411319661ccb8339065517d84b22
leozizz <leonardocps2015@gmail.com>
2023-02-15 11:55:48 -0300

    Finalizando estrutura e estilização

--- src/componentes/Conteudo/SecaoContato/index.js
+++ src/componentes/Conteudo/SecaoContato/index.js
@@ -2,22 +2,40 @@ import React from "react";
 
 export default function SecaoContato(){
     return(
-        <section>
-            <div className="container">
+        <section className="secao-contato">
+            <div className="container limitar-secao">
                 <h2>FALE CONOSCO</h2>
                 <p>Não perca tempo, venha conhecer uma de nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento.</p>
-                <div className="infoSocial">
+                <div className="info-social">
                     <div className="contato">
                         <h3>Contato</h3>
-                        <p><img src="assets/local.png"></img>Nova Iguaçu, RJ</p>
-                        <p><img src="assets/telefone.png"></img>(21) 9999-9999</p>
-                        <p><img src="assets/email.png"></img>contato@oticavida.com</p>
+                        <div className="item-container">
+                            <img src="assets/local.png"></img>
+                            <p>Nova Iguaçu, RJ</p>
+                        </div>
+                        <div className="item-container">
+                            <img src="assets/telefone.png"></img>
+                            <p>(21) 9999-9999</p>
+                        </div>
+                        <div className="item-container">
+                            <img src="assets/email.png"></img>
+                            <p>contato@oticavida.com</p>
+                        </div>                        
                     </div>
-                    <div className="redesSociais">
+                    <div className="redes-sociais">
                         <h3>Nossas Redes Sociais</h3>
-                        <p><img src="assets/fb.png"></img>/OticaVida</p>
-                        <p><img src="assets/ig.png"></img>@oticavidarj</p>
-                        <p><img src="assets/tt.png"></img>@oticavidarj</p>                       
+                        <div className="item-container">
+                            <img src="assets/fb.png"></img>
+                            <p>/OticaVida</p>
+                        </div>
+                        <div className="item-container">
+                            <img src="assets/ig.png"></img>
+                            <p>@oticavidarj</p>
+                        </div>
+                        <div className="item-container">
+                            <img src="assets/tt.png"></img>
+                            <p>@oticavidarj</p>                       
+                        </div>
                     </div>
                 </div>
             </div>
