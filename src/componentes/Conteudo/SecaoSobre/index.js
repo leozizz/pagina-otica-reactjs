15e6ea44ef5923e85f40860f49df9174801716b0
leozizz <leonardocps2015@gmail.com>
2023-02-15 10:44:08 -0300

    Estilizando Seção Sobre

--- src/componentes/Conteudo/SecaoSobre/index.js
+++ src/componentes/Conteudo/SecaoSobre/index.js
@@ -2,22 +2,22 @@ import React from "react";
 
 export default function SecaoSobre(){
     return(
-        <section>
-            <div className="container">
+        <section className="secao-sobre">
+            <div className="container limitar-secao">
                 <h2>QUEM SOMOS NÓS?</h2>
                 <p>Fundada em 2001, em Nova Iguaçu - Rio de janeiro, a Óticas vida iniciou suas atividades focada no atendimento ao público de renda mais baixa, sempre com o objetivo de proporcionar ao cliente bom atendimento, qualidade e preço baixo.</p>
 
-                <div className="cardContainer">
+                <div className="card-container">
+                    <img src="assets/loja.png"></img>
                     <div className="card">
-                        <img src="assets/loja.png"></img>
                         <h3>NOSSAS FILIAIS</h3>
                         <p>Hoje temos mais de 20 filiais pelo Brasil e na América</p>
                     </div>
                     <div className="card">
-                        <img src="assets/atendimento.png"></img>
                         <h3>ATENDIMENTO FLEXIVEL</h3>
                         <p>Nossa equipe é treinada para te atender</p>
                     </div>
+                    <img src="assets/atendimento.png"></img>
                 </div>
             </div>
         </section>
